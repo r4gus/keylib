070e36f71968d91444139dae9bf4dc1c5dd0b2ef
David Pierre Sugar <david@thesugar.de>
2022-11-07 16:24:21 +0100

    attestationObject encoding function and test added

--- src/attestation_object.zig
+++ src/attestation_object.zig
@@ -136,11 +136,65 @@ pub const AuthData = struct {
         // TODO: also encode extensions
     }
 };
+/// WebAuthn Attestation Statement Format Identifiers
+///
+/// https://www.w3.org/TR/webauthn/#sctn-defined-attestation-formats
+pub const Fmt = enum {
+    /// The "packed" attestation statement format is a WebAuthn-optimized format for attestation. It uses a very compact but still extensible encoding method. This format is implementable by authenticators with limited resources (e.g., secure elements).
+    packed_,
+    /// The TPM attestation statement format returns an attestation statement in the same format as the packed attestation statement format, although the rawData and signature fields are computed differently.
+    tpm,
+    /// Platform authenticators on versions "N", and later, may provide this proprietary "hardware attestation" statement.
+    android_key,
+    /// Android-based platform authenticators MAY produce an attestation statement based on the Android SafetyNet API.
+    android_safetynet,
+    /// Used with FIDO U2F authenticators
+    fido_u2f,
+    /// Used with Apple devices' platform authenticators
+    apple,
+    /// Used to replace any authenticator-provided attestation statement when a WebAuthn Relying Party indicates it does not wish to receive attestation information.
+    none,
 
+    pub fn toString(self: @This()) []const u8 {
+        return switch (self) {
+            .packed_ => "packed",
+            .tpm => "tpm",
+            .android_key => "android-key",
+            .android_safetynet => "android-safetynet",
+            .fido_u2f => "fido-u2f",
+            .apple => "apple",
+            .none => "none",
+        };
+    }
+};
+
+/// https://www.w3.org/TR/webauthn/#sctn-attestation
 pub const AttestationObject = struct {
-    fmt: []const u8,
+    fmt: Fmt,
     att_stmt: AttStmt,
     auth_data: AuthData,
+
+    pub fn encode(self: *const @This(), allocator: Allocator) ![]u8 {
+        var ret = std.ArrayList(u8).init(allocator);
+        errdefer ret.deinit();
+        var wret = ret.writer();
+        var ad = std.ArrayList(u8).init(allocator);
+        defer ad.deinit();
+        var wad = ad.writer();
+        var pairs: [3]Pair = undefined;
+
+        pairs[0] = Pair.new(DataItem{ .text = "fmt" }, DataItem{ .text = self.fmt.toString() });
+
+        pairs[1] = Pair.new(DataItem{ .text = "attStmt" }, self.att_stmt.toCbor());
+
+        try self.auth_data.encode(wad);
+        pairs[2] = Pair.new(DataItem{ .text = "authData" }, DataItem{ .bytes = ad.items });
+
+        const di = DataItem{ .map = pairs[0..] };
+        try cbor.encode(wret, &di);
+
+        return ret.toOwnedSlice();
+    }
 };
 
 // see: https://www.w3.org/TR/webauthn/#sctn-defined-attestation-formats
@@ -220,4 +274,41 @@ test "authData encoding" {
     try ad.encode(w);
 
     try std.testing.expectEqualSlices(u8, a.items, "\x21\x09\x18\x5f\x69\x3a\x01\xea\x1a\x26\x41\xf8\x2d\x52\xfb\xae\xee\x0a\x4f\x47\xe3\x37\x4d\xfe\xf8\x70\x83\x8d\xe4\x9b\x0e\x97\x41\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x40\xb3\xf8\xcd\xb1\x80\x20\x91\x76\xfa\x20\x1a\x51\x6d\x1b\x42\xf8\x02\xa8\x0d\xaf\x48\xd0\x37\x88\x21\xa6\xfb\xdd\x52\xde\x16\xb7\xef\xf6\x22\x25\x72\x43\x8d\xe5\x85\x7e\x70\xf9\xef\x05\x80\xe9\x37\xe3\x00\xae\xd0\xdf\xf1\x3f\xb6\xa3\x3e\xc3\x8b\x81\xca\xd0\xa5\x01\x02\x03\x26\x20\x01\x21\x58\x20\xd9\xf4\xc2\xa3\x52\x13\x6f\x19\xc9\xa9\x5d\xa8\x82\x4a\xb5\xcd\xc4\xd5\x63\x1e\xbc\xfd\x5b\xdb\xb0\xbf\xff\x25\x36\x09\x12\x9e\x22\x58\x20\xef\x40\x4b\x88\x07\x65\x57\x60\x07\x88\x8a\x3e\xd6\xab\xff\xb4\x25\x7b\x71\x23\x55\x33\x25\xd4\x50\x61\x3c\xb5\xbc\x9a\x3a\x52");
+}
+
+test "attestationObject encoding - no attestation" {
+    const allocator = std.testing.allocator;
+
+    const acd = AttestedCredentialData{
+        .aaguid = .{ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 },
+        .credential_length = 0x0040,
+        .credential_id = &.{ 0xb3, 0xf8, 0xcd, 0xb1, 0x80, 0x20, 0x91, 0x76, 0xfa, 0x20, 0x1a, 0x51, 0x6d, 0x1b, 0x42, 0xf8, 0x02, 0xa8, 0x0d, 0xaf, 0x48, 0xd0, 0x37, 0x88, 0x21, 0xa6, 0xfb, 0xdd, 0x52, 0xde, 0x16, 0xb7, 0xef, 0xf6, 0x22, 0x25, 0x72, 0x43, 0x8d, 0xe5, 0x85, 0x7e, 0x70, 0xf9, 0xef, 0x05, 0x80, 0xe9, 0x37, 0xe3, 0x00, 0xae, 0xd0, 0xdf, 0xf1, 0x3f, 0xb6, 0xa3, 0x3e, 0xc3, 0x8b, 0x81, 0xca, 0xd0 },
+        .credential_public_key = try Ecdsa.PublicKey.fromSec1("\x04\xd9\xf4\xc2\xa3\x52\x13\x6f\x19\xc9\xa9\x5d\xa8\x82\x4a\xb5\xcd\xc4\xd5\x63\x1e\xbc\xfd\x5b\xdb\xb0\xbf\xff\x25\x36\x09\x12\x9e\xef\x40\x4b\x88\x07\x65\x57\x60\x07\x88\x8a\x3e\xd6\xab\xff\xb4\x25\x7b\x71\x23\x55\x33\x25\xd4\x50\x61\x3c\xb5\xbc\x9a\x3a\x52"),
+    };
+
+    const ad = AuthData{
+        .rp_id_hash = .{ 0x21, 0x09, 0x18, 0x5f, 0x69, 0x3a, 0x01, 0xea, 0x1a, 0x26, 0x41, 0xf8, 0x2d, 0x52, 0xfb, 0xae, 0xee, 0x0a, 0x4f, 0x47, 0xe3, 0x37, 0x4d, 0xfe, 0xf8, 0x70, 0x83, 0x8d, 0xe4, 0x9b, 0x0e, 0x97 },
+        .flags = Flags{
+            .up = 1,
+            .rfu1 = 0,
+            .uv = 0,
+            .rfu2 = 0,
+            .at = 1,
+            .ed = 0,
+        },
+        .sign_count = 0,
+        .attested_credential_data = acd,
+        .extensions = &.{},
+    };
+
+    const ao = AttestationObject{
+        .fmt = Fmt.none,
+        .att_stmt = AttStmt{ .none = true },
+        .auth_data = ad,
+    };
+
+    const data = try ao.encode(allocator);
+    defer allocator.free(data);
+
+    try std.testing.expectEqualSlices(u8, data, "\xa3\x63\x66\x6d\x74\x64\x6e\x6f\x6e\x65\x67\x61\x74\x74\x53\x74\x6d\x74\xa0\x68\x61\x75\x74\x68\x44\x61\x74\x61\x58\xc4\x21\x09\x18\x5f\x69\x3a\x01\xea\x1a\x26\x41\xf8\x2d\x52\xfb\xae\xee\x0a\x4f\x47\xe3\x37\x4d\xfe\xf8\x70\x83\x8d\xe4\x9b\x0e\x97\x41\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x40\xb3\xf8\xcd\xb1\x80\x20\x91\x76\xfa\x20\x1a\x51\x6d\x1b\x42\xf8\x02\xa8\x0d\xaf\x48\xd0\x37\x88\x21\xa6\xfb\xdd\x52\xde\x16\xb7\xef\xf6\x22\x25\x72\x43\x8d\xe5\x85\x7e\x70\xf9\xef\x05\x80\xe9\x37\xe3\x00\xae\xd0\xdf\xf1\x3f\xb6\xa3\x3e\xc3\x8b\x81\xca\xd0\xa5\x01\x02\x03\x26\x20\x01\x21\x58\x20\xd9\xf4\xc2\xa3\x52\x13\x6f\x19\xc9\xa9\x5d\xa8\x82\x4a\xb5\xcd\xc4\xd5\x63\x1e\xbc\xfd\x5b\xdb\xb0\xbf\xff\x25\x36\x09\x12\x9e\x22\x58\x20\xef\x40\x4b\x88\x07\x65\x57\x60\x07\x88\x8a\x3e\xd6\xab\xff\xb4\x25\x7b\x71\x23\x55\x33\x25\xd4\x50\x61\x3c\xb5\xbc\x9a\x3a\x52");
 }
