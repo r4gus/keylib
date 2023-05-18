c29b21784c0ab643752e0e390a45be047aa3addd
David P. Sugar <david@thesugar.de>
2023-05-19 00:53:17 +0200

    removed files

--- platform-auth/linux/.Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/sugar/Projects/candy-stick-nrf/libs/fido2/platform-auth/linux/Module.symvers :=  scripts/mod/modpost -m -a     -o /home/sugar/Projects/candy-stick-nrf/libs/fido2/platform-auth/linux/Module.symvers -T /home/sugar/Projects/candy-stick-nrf/libs/fido2/platform-auth/linux/modules.order -i Module.symvers -e 
