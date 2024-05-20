ac8e6a182a8bdd0eac27b99e1ad5dde2da839633
David P. Sugar <david@thesugar.de>
2024-05-20 12:59:22 +0200

    User and RelyingParty de/serialization tests added

--- lib/common/RelyingParty.zig
+++ lib/common/RelyingParty.zig
@@ -1,5 +1,6 @@
 //! Representation of a relying party
 
+const std = @import("std");
 const cbor = @import("zbor");
 const dt = @import("data_types.zig");
 
@@ -23,4 +24,22 @@ pub fn new(
         .id = (try dt.ABS128T.fromSlice(id)).?,
         .name = try dt.ABS64T.fromSlice(name),
     };
+}
+
+test "RelyingParty test #1" {
+    const pkorg = try new("passkey.org", "Yubico Demo");
+    const expected = "\xa2\x62\x69\x64\x6b\x70\x61\x73\x73\x6b\x65\x79\x2e\x6f\x72\x67\x64\x6e\x61\x6d\x65\x6b\x59\x75\x62\x69\x63\x6f\x20\x44\x65\x6d\x6f";
+
+    var arr = std.ArrayList(u8).init(std.testing.allocator);
+    defer arr.deinit();
+
+    try cbor.stringify(pkorg, .{}, arr.writer());
+
+    try std.testing.expectEqualSlices(u8, expected, arr.items);
+
+    const di = try cbor.DataItem.new(expected);
+    const pkorg2 = try cbor.parse(@This(), di, .{});
+
+    try std.testing.expectEqualStrings(pkorg.id.get(), pkorg2.id.get());
+    try std.testing.expectEqualStrings(pkorg.name.?.get(), pkorg2.name.?.get());
 }
--- lib/common/User.zig
+++ lib/common/User.zig
@@ -1,7 +1,7 @@
 //! Representation of a user
 
-const cbor = @import("zbor");
 const std = @import("std");
+const cbor = @import("zbor");
 const dt = @import("data_types.zig");
 
 /// The user handle of the user account. A user handle is an opaque byte
@@ -28,4 +28,23 @@ pub fn new(
         .name = try dt.ABS64T.fromSlice(name),
         .displayName = try dt.ABS64T.fromSlice(displayName),
     };
+}
+
+test "User test #1" {
+    const klaus = try new("\x0c\x43\x0e\xff\xff\x5f\x5f\x5f\x44\x45\x4d\x4f", "klaus", "klaus");
+    const expected = "\xa3\x62\x69\x64\x4c\x0c\x43\x0e\xff\xff\x5f\x5f\x5f\x44\x45\x4d\x4f\x64\x6e\x61\x6d\x65\x65\x6b\x6c\x61\x75\x73\x6b\x64\x69\x73\x70\x6c\x61\x79\x4e\x61\x6d\x65\x65\x6b\x6c\x61\x75\x73";
+
+    var arr = std.ArrayList(u8).init(std.testing.allocator);
+    defer arr.deinit();
+
+    try cbor.stringify(klaus, .{}, arr.writer());
+
+    try std.testing.expectEqualSlices(u8, expected, arr.items);
+
+    const di = try cbor.DataItem.new(expected);
+    const klaus2 = try cbor.parse(@This(), di, .{});
+
+    try std.testing.expectEqualStrings(klaus.id.get(), klaus2.id.get());
+    try std.testing.expectEqualStrings(klaus.name.?.get(), klaus2.name.?.get());
+    try std.testing.expectEqualStrings(klaus.displayName.?.get(), klaus2.displayName.?.get());
 }
